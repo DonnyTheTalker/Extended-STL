dd3070bb905305f8a12de024c5ea86e88fcc68d8
DonnyTheTalker <maks.egorov.55@mail.ru>
2020-05-28 18:55:50 +0300

    Find algorithms + Merge Sort Added

--- ExtendedSTL/ExtendedSTL/ExtendedSTL.cpp
+++ ExtendedSTL/ExtendedSTL/ExtendedSTL.cpp
@@ -5,11 +5,14 @@
 #include <vector> 
 #include <chrono>
 #include <random>
+#include <typeinfo>
 
 namespace eStd
 {
 	template <typename T>
 	using Value_type = typename T::value_type;
+	template <typename T>
+	using Iterator_type = typename T::iterator;
 
 	std::random_device rd;
 	std::mt19937 g(rd());
@@ -47,6 +50,55 @@ namespace eStd
 	{
 		QuickSortRange(container.begin(), container.end(), comp);
 	}
+
+	template <typename RandIt, typename Comp = std::less<> >
+	void StableQuickSortRange(RandIt begin, RandIt end, Comp comp = Comp())
+	{
+		int dist = std::distance(begin, end);
+		if (dist <= 1) return;
+
+		auto pivot = *(std::next(begin, dist / 2));
+		auto mid1 = std::stable_partition(begin, end, [&](auto& lhs) {
+			return comp(lhs, pivot);
+			});
+		auto mid2 = std::stable_partition(mid1, end, [&](auto& rhs) {
+			return !comp(pivot, rhs);
+			});
+
+		StableQuickSortRange(begin, mid1, comp);
+		StableQuickSortRange(mid2, end, comp);
+	}
+
+	template <typename T, typename Comp = std::less<> >
+	void StableQuickSort(T& container, Comp comp = Comp())
+	{
+		StableQuickSortRange(container.begin(), container.end(), comp);
+	} 
+
+	template <typename RandIt, typename Comp = std::less<> >
+	void MergeSortRange(RandIt begin, RandIt end, Comp comp = Comp())
+	{
+		int dist = std::distance(begin, end);
+		if (dist <= 1) return;
+
+		dist /= 2;
+		
+		auto mid1 = std::next(begin, dist);
+		auto mid2 = std::next(mid1, 1);
+
+		MergeSortRange(begin, mid1, comp);
+		MergeSortRange(mid2, end, comp);
+
+		std::vector <Value_type<RandIt> > temp;
+		std::merge(begin, mid1, mid2, end, std::back_inserter(temp), comp);
+		std::copy(temp.begin(), temp.end(), begin);
+	}
+
+	template <typename T, typename Comp = std::less<> >
+	void MergeSort(T& container, Comp comp = Comp())
+	{
+		MergeSortRange(container.begin(), container.end(), comp);
+	}
 }
 
 namespace eStd
@@ -77,12 +129,25 @@ namespace eStd
 	} 
 
 	template <typename T>
-	bool ContainsWBSearch(const T& container, Value_type<T> value)
+	bool ContainsWBSearch(const T& container, const Value_type<T>& value)
 	{
 		return std::binary_search(container.begin(), container.end(), value);
 	}
 
-	
+	template <typename T> 
+	Iterator_type<T> FindUnsorted(T& container, const Value_type<T>& value)
+	{
+		return std::find(container.begin(), container.end(), value);
+	}
+
+	template <typename T>
+	Iterator_type<T> FindSorted(T& container, const Value_type<T>& value)
+	{
+		auto it = std::lower_bound(container.begin(), container.end(), value); 
+		if (it == container.end() || *it != value)
+			return container.end();
+		return it;
+	} 
 
 }
 
@@ -129,9 +194,4 @@ namespace eStd
 			return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
 		}
 	};
-}
-
-int main()
-{
-	std::cout << std::boolalpha << eStd::ContainsWBSearch(std::vector <int> {1, 2, 3}, 4);
-}
+} 
