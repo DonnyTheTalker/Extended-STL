d215541a1e9e8d2892470bf1a9722dfe2abb636d
DonnyTheTalker <maks.egorov.55@mail.ru>
2020-05-28 17:52:43 +0300

    Sorts Added

--- ExtendedSTL/ExtendedSTL/ExtendedSTL.cpp
+++ ExtendedSTL/ExtendedSTL/ExtendedSTL.cpp
@@ -7,13 +7,50 @@
 #include <random>
 
 namespace eStd
-{  
+{
 	template <typename T>
 	using Value_type = typename T::value_type;
 
 	std::random_device rd;
 	std::mt19937 g(rd());
+}
 
+namespace eStd
+{
+	template <typename T, typename Comp = std::less<> >
+	void InsertionSort(T& container, Comp comp = Comp())
+	{
+		for (auto it = container.begin(); it != container.end(); it++)
+			std::rotate(std::upper_bound(container.begin(), it, *it, comp), it, std::next(it, 1));
+	}
+
+	template <typename RandIt, typename Comp = std::less<> >
+	void QuickSortRange(RandIt begin, RandIt end, Comp comp = Comp())
+	{
+		int dist = std::distance(begin, end);
+		if (dist <= 1) return;
+
+		auto pivot = *(std::next(begin, dist / 2));
+		auto mid1 = std::partition(begin, end, [&](auto& lhs) {
+			return comp(lhs, pivot);
+			});
+		auto mid2 = std::partition(mid1, end, [&](auto& rhs) {
+			return !comp(pivot, rhs);
+			});
+
+		QuickSortRange(begin, mid1, comp);
+		QuickSortRange(mid2, end, comp);
+	}
+
+	template <typename T, typename Comp = std::less<> >
+	void QuickSort(T& container, Comp comp = Comp())
+	{
+		QuickSortRange(container.begin(), container.end(), comp);
+	}
+}
+
+namespace eStd
+{ 
 	template <typename T>
 	void Print(const T& container)
 	{
@@ -29,7 +66,7 @@ namespace eStd
 	template <typename T, typename Comp = std::less<> >
 	void RemoveDuplicateUnsorted(T& container, Comp comp = Comp())
 	{
-		std::sort(container.begin(), container.end(), comp);
+		QuickSort(container, comp);
 		RemoveDuplicateSorted(container);
 	} 
 
@@ -37,7 +74,16 @@ namespace eStd
 	void Shuffle(T& container)
 	{
 		std::shuffle(container.begin(), container.end(), g);
+	} 
+
+	template <typename T>
+	bool ContainsWBSearch(const T& container, Value_type<T> value)
+	{
+		return std::binary_search(container.begin(), container.end(), value);
 	}
+
+	
+
 }
 
 namespace eStd
@@ -83,4 +129,9 @@ namespace eStd
 			return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
 		}
 	};
+}
+
+int main()
+{
+	std::cout << std::boolalpha << eStd::ContainsWBSearch(std::vector <int> {1, 2, 3}, 4);
 }
